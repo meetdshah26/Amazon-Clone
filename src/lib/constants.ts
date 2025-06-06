b9e3ce0c163906756a29daddd0b566743958d42e
tai-meetshah <theappideas.meet@gmail.com>
2025-06-06 14:49:45 +0530

    Code setup

---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+export const SENDER_NAME = process.env.SENDER_NAME || 'support'
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev'
+
+export const USER_ROLES = ['Admin', 'User']
+export const COLORS = ['Gold', 'Green', 'Red']
+export const THEMES = ['Light', 'Dark', 'System']
+export const APP_NAME = process.env.APP_NAME || 'Test'
+export const APP_DESCRIPTION = process.env.APP_NAME || "Resend Starter";
+export const APP_SLOGAN = process.env.APP_NAME || "Resend Starter";
